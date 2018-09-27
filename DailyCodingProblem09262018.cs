c1ebc2817812457387bed2cc2318f8dcfc5f033c
AnotherCasualCoder <marcelo_de_barros@hotmail.com>
2018-09-26 23:05:36 -0700

    Daily Coding Problem 9/26/2018

---
+++ DailyCodingProblem09262018.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingProblem
+{
+	class DailyCodingProblem09262018
+	{
+		public long StaircaseUniqueWays(int N, int[] steps)
+		{
+			long[] stair = new long[N + 1];
+
+			stair[0] = 1;
+
+			for (int i = 1; i <= N; i++)
+			{
+				long val = 0;
+				for (int j = 0; j < steps.Length; j++)
+					if (i - steps[j] >= 0)
+						val += stair[i - steps[j]];
+				stair[i] = val;
+			}
+
+			return stair[N];
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -10,9 +10,16 @@ namespace DailyCodingProblem
 	{
 		static void Main(string[] args)
 		{
+			/*
 			DailyCodingProblem09252018 dcp = new DailyCodingProblem09252018();
 			dcp.CreateTrie(new string[] { "dog", "deer", "deal", "doggy", "derail", "abs" });
 			dcp.PrintMatches(args[0]);
+			*/
+
+			DailyCodingProblem09262018 dcp = new DailyCodingProblem09262018();
+			int N = 432;
+			int[] steps = { 2, 3, 5 };
+			Console.WriteLine(dcp.StaircaseUniqueWays(N, steps));
 		}
 	}
 }
