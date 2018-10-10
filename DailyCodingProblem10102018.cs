1093129e22fcb3aab0b43d65bbc6b8a4b149775c
AnotherCasualCoder <marcelo_de_barros@hotmail.com>
2018-10-10 11:49:06 -0700

    Daily Coding Problem

---
+++ DailyCodingProblem10102018.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DailyCodingProblem
+{
+	class DailyCodingProblem10102018
+	{
+		private MyList list = null;
+
+		public DailyCodingProblem10102018(int n)
+		{
+			Random rd = new Random();
+
+			list = new MyList(rd.Next(0, 100));
+			MyList current = list;
+			for (int i = 1; i < n; i++)
+			{
+				current.next = new MyList(rd.Next(0, 100));
+				current = current.next;
+			}
+		}
+
+		public void DeleteK(int k)
+		{
+			PrintList();
+
+			MyList previousBehind = null;
+			MyList currentBehind = null;
+
+			int count = 1;
+			for (MyList current = list; current != null; current = current.next)
+			{
+				if (count >= k)
+				{
+					if (currentBehind == null)
+					{
+						currentBehind = list;
+					}
+					else
+					{
+						previousBehind = currentBehind;
+						currentBehind = currentBehind.next;
+					}
+				}
+				count++;
+			}
+
+			if (previousBehind == null)
+			{
+				list = currentBehind.next;
+			}
+			else
+			{
+				previousBehind.next = currentBehind.next;
+			}
+
+			PrintList();
+		}
+
+		public void PrintList()
+		{
+			for (MyList l = list; l != null; l = l.next)
+			{
+				Console.Write("{0} ", l.val);
+			}
+			Console.WriteLine();
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace DailyCodingProblem
 	{
 		static void Main(string[] args)
 		{
+			/*
 			DailyCodingProblem10082018 dcp = new DailyCodingProblem10082018();
 			LockTree tree = dcp.BuildTree();
 			dcp.LockUnlock(22, true, tree);
@@ -18,12 +19,17 @@ namespace DailyCodingProblem
 			dcp.LockUnlock(22, false, tree);
 			dcp.LockUnlock(10, true, tree);
 			dcp.LockUnlock(10, false, tree);
+			*/
+
 			/*
 			DailyCodingProblem10092018 dcp = new DailyCodingProblem10092018();
 			string regex = args[0];
 			string input = args[1];
 			Console.WriteLine(dcp.RegEx(regex, input));
 			*/
+
+			DailyCodingProblem10102018 dcp = new DailyCodingProblem10102018(Int32.Parse(args[0]));
+			dcp.DeleteK(Int32.Parse(args[1]));
 		}
 	}
 }
